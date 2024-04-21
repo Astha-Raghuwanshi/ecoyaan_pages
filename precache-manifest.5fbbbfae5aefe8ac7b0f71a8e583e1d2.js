de39bf900b035c4885c8de813b430cc1a6f56e8c
Astha <astharaghuwanshiofficial@gmail.com>
2024-04-22 00:30:29 +0530

    Updates

---
+++ precache-manifest.5fbbbfae5aefe8ac7b0f71a8e583e1d2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "363b6e4f3c6c10a68d42c8b3b19478e2",
+    "url": "/ecoyaan_pages/index.html"
+  },
+  {
+    "revision": "d7f86d37d71d42ee3394",
+    "url": "/ecoyaan_pages/static/css/2.4ee5ca5c.chunk.css"
+  },
+  {
+    "revision": "c117653aca9231fb9b69",
+    "url": "/ecoyaan_pages/static/css/main.b069127b.chunk.css"
+  },
+  {
+    "revision": "d7f86d37d71d42ee3394",
+    "url": "/ecoyaan_pages/static/js/2.6d6ea4da.chunk.js"
+  },
+  {
+    "revision": "4c27c027f65f16775cc772cd65c92852",
+    "url": "/ecoyaan_pages/static/js/2.6d6ea4da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c117653aca9231fb9b69",
+    "url": "/ecoyaan_pages/static/js/main.4a539ee3.chunk.js"
+  },
+  {
+    "revision": "08ee0a3e64de9201646f",
+    "url": "/ecoyaan_pages/static/js/runtime-main.7fad1890.js"
+  },
+  {
+    "revision": "fa37179b849830c1fa72c70d0a413363",
+    "url": "/ecoyaan_pages/static/media/pexels-alena-koval-886521.fa37179b.jpg"
+  },
+  {
+    "revision": "fa01e82acbb0d6ef4808a2950fd82b7d",
+    "url": "/ecoyaan_pages/static/media/pexels-karolina-grabowska-4202926.fa01e82a.jpg"
+  },
+  {
+    "revision": "2e73840755540d4aa1f7907622eab49f",
+    "url": "/ecoyaan_pages/static/media/pexels-ready-made-3850512.2e738407.jpg"
+  },
+  {
+    "revision": "a0cc55378f6d11b9083bc22c2c23cc05",
+    "url": "/ecoyaan_pages/static/media/pexels-scott-webb-305821.a0cc5537.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/ecoyaan_pages/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/ecoyaan_pages/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/ecoyaan_pages/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/ecoyaan_pages/static/media/slick.f97e3bbf.svg"
+  }
+]);
